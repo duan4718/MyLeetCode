96a4e23fa75acc8b6a058cd2a5adaa9412bb3965
StephenDurant <duan4718@hotmail.com>
2018-06-08 18:43:24 +0800

    Update P7_ReverseInteger.py

--- P7_ReverseInteger.py
+++ P7_ReverseInteger.py
@@ -13,7 +13,10 @@ Example 3:
 Note:
   Assume we are dealing with an environment which could only store integers
 within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose of
-this problem, assume that your function returns 0 when the reversed integer overflows.:"""
+this problem, assume that your function returns 0 when the reversed integer overflows.
+
+rate:easy
+"""
 
 
 class Solution:
